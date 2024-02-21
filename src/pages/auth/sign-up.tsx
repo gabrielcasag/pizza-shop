c6b59bb54fe94999d2a4d6e8755952e386b8e670
gabrielcasag <gaspericasag@gmail.com>
2024-02-21 19:42:11 -0300

    fix: typo on toast

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -32,7 +32,7 @@ export function SignUp() {
 
       await new Promise((resolve) => setTimeout(resolve, 400))
 
-      toast.success('Restaurante cadastrado com sucesso!.', {
+      toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
           onClick: () => {
