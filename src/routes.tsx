738b5490a20b3044590abad8acc8be9da329140a
gabrielcasag <gaspericasag@gmail.com>
2024-02-22 21:46:11 -0300

    chore: create orders list component

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -13,33 +14,16 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
     ],
   },
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      {
-        path: '/signin',
-        element: <SignIn />,
-      },
-      {
-        path: '/signup',
-        element: <SignUp />,
-      },
+      { path: '/signin', element: <SignIn /> },
+      { path: '/signup', element: <SignUp /> },
     ],
   },
-
-  // {
-  //   path: '/',
-  //   element: <Dashboard />,
-  // },
-  // {
-  //   path: '/signin',
-  //   element: <SignIn />,
-  // },
 ])
